8c60528191a6c119267e918c55ec62cee9252fb7
DeTovelli <detovelli@gmail.com>
2023-05-05 00:36:48 +0300

    Hot_fix_05.05.23

--- js/createRequest.js
+++ js/createRequest.js
@@ -2,21 +2,27 @@
 
 const createRequest = (options = {}) => {
   const xhr = new XMLHttpRequest();
-  xhr.open("POST", options.url, true);
+  xhr.open(options.method || "POST", options.url, true);
   xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
   xhr.send(options.params);
-  xhr.onreadystatechange =  () => {
-    if((xhr.readyState === 4) && (xhr.status === 200)) {
-      console.log(xhr.responseText);
-      console.log(JSON.parse(xhr.responseText))
-      const response =  JSON.parse(xhr.responseText);
-      for (let key in response) {
-        if (response[key].err) {
-          alert(`Ошибка обращения к базе данных ${response[key].err}: ${response[key].errMessage}`);
-          return;
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState === 4) {
+      if (xhr.status === 200) {
+        try {
+          const response = JSON.parse(xhr.responseText);
+          for (let key in response) {
+            if (response[key].err) {
+              alert(`Ошибка обращения к базе данных ${response[key].err}: ${response[key].errMessage}`);
+              return;
+            }
+          }
+          options.callback(response);
+        } catch (err) {
+          alert(`Ошибка разбора ответа сервера: ${err.message}`);
         }
+      } else {
+        alert(`Ошибка запроса к серверу: ${xhr.status} ${xhr.statusText}`);
       }
-      options.callback(response);
     }
-  }
-}
+  };
+};
--- js/hall.js
+++ js/hall.js
@@ -1,45 +1,56 @@
-"use strict"
+"use strict";
 
-const  selectSeanse = JSON.parse(localStorage.selectSeanse)
-console.log(selectSeanse)
+const selectSeanse = JSON.parse(localStorage.selectSeanse);
+console.log(selectSeanse);
 
-document.addEventListener("DOMContentLoaded", ()=>{
-  const buttonAcceptin = document.querySelector('.acceptin-button'); // Кнопка "Бронировать"
+document.addEventListener("DOMContentLoaded", () => {
+  const buttonAcceptin = document.querySelector('.acceptin-button');
+  const buyingInfoTitle = document.querySelector('.buying__info-title');
+  const buyingInfoStart = document.querySelector('.buying__info-start');
+  const buyingInfoHall = document.querySelector('.buying__info-hall');
+  const priceStandart = document.querySelector('.price-standart');
+  const confStepWrapper = document.querySelector('.conf-step__wrapper');
 
-document.querySelector('.buying__info-title').innerHTML = selectSeanse.filmName;
-document.querySelector('.buying__info-start').innerHTML = `Начало сеанса ${selectSeanse.seanceTime}`;
-document.querySelector('.buying__info-hall').innerHTML = selectSeanse.hallName;
-document.querySelector('.price-standart').innerHTML = selectSeanse.priceStandart;
-console.log(`event=get_hallConfig&timestamp=${selectSeanse.seanceTimeStamp}&hallId=${selectSeanse.hallId}&seanceId=${selectSeanse.seanceId}`)
-createRequest({
-  url: "http://f0769682.xsph.ru/",
-  params:  `event=get_hallConfig&timestamp=${selectSeanse.seanceTimeStamp}&hallId=${selectSeanse.hallId}&seanceId=${selectSeanse.seanceId}`,
-  callback: (resp) => {
-    console.log(resp);
-    if (resp) {
-      selectSeanse.hallConfig = resp
+  buyingInfoTitle.innerHTML = selectSeanse.filmName;
+  buyingInfoStart.innerHTML = `Начало сеанса ${selectSeanse.seanceTime}`;
+  buyingInfoHall.innerHTML = selectSeanse.hallName;
+  priceStandart.innerHTML = selectSeanse.priceStandart;
+
+  const params = `event=get_hallConfig&timestamp=${selectSeanse.seanceTimeStamp}&hallId=${selectSeanse.hallId}&seanceId=${selectSeanse.seanceId}`;
+
+  createRequest({
+    url: "http://f0769682.xsph.ru/",
+    params,
+    callback: (resp) => {
+      console.log(resp);
+      if (resp) {
+        selectSeanse.hallConfig = resp;
+      }
+      confStepWrapper.innerHTML = selectSeanse.hallConfig;
+      const chairs = [...document.querySelectorAll('.conf-step__row .conf-step__chair')];
+      let chairsSelected = [...document.querySelectorAll('.conf-step__row .conf-step__chair_selected')];
+      if (chairsSelected.length) {
+        buttonAcceptin.removeAttribute("disabled");
+      } else {
+        buttonAcceptin.setAttribute("disabled", true);
+      }
+
+      chairs.forEach((chair) => {
+        chair.addEventListener('click', (event) => {
+          if (event.target.classList.contains('conf-step__chair_taken')) {
+            return;
+          }
+          event.target.classList.toggle('conf-step__chair_selected');
+          chairsSelected = [...document.querySelectorAll('.conf-step__row .conf-step__chair_selected')];
+          if (chairsSelected.length) {
+            buttonAcceptin.removeAttribute("disabled");
+          } else {
+            buttonAcceptin.setAttribute("disabled", true);
+          }
+        });
+      });
     }
-    document.querySelector('.conf-step__wrapper').innerHTML = selectSeanse.hallConfig;
-    const chairs = Array.from(document.querySelectorAll('.conf-step__row .conf-step__chair')); // Все кресла
-  let chairsSelected = Array.from(document.querySelectorAll('.conf-step__row .conf-step__chair_selected')); // Выбранные кресла
-  if (chairsSelected.length) {
-    buttonAcceptin.removeAttribute("disabled");
-  } else {
-    buttonAcceptin.setAttribute("disabled", true);
-  }
-
-  // We hang the onclick event on the chairs
-  chairs.forEach(chair => chair.addEventListener('click', (event) => {
-    if (event.target.classList.contains('conf-step__chair_taken')) {return}; // Прерываем выполнение если клик был по забронированному месту
-    event.target.classList.toggle('conf-step__chair_selected');
-    chairsSelected = Array.from(document.querySelectorAll('.conf-step__row .conf-step__chair_selected'));
-    if (chairsSelected.length) {
-      buttonAcceptin.removeAttribute("disabled");
-    } else {
-      buttonAcceptin.setAttribute("disabled", true);
-    }    
-  }));
-  }})
+  });
   
   // We hang the onclick event on the button
   buttonAcceptin.addEventListener("click", (event) => {
@@ -52,12 +63,12 @@ createRequest({
       for (let j=0; j < spanPlaces.length; j++) {
         if (spanPlaces[j].classList.contains("conf-step__chair_selected")) {
           // Determine the type of chair chosen
-          const typePlace = (spanPlaces[j].classList.contains("conf-step__chair_standart")) ? "standart" : "vip"
+          const typePlace = (spanPlaces[j].classList.contains("conf-step__chair_standart")) ? "standart" : "vip";
           selectedPlaces.push({
             "row": i+1,
             "place": j+1,
             "type":  typePlace
-          })
+          });
         }
       }
     }
@@ -67,11 +78,11 @@ createRequest({
     selectSeanse.hallConfig = configurationHall;
     selectSeanse.salesPlaces = selectedPlaces;
     localStorage.clear();
-    localStorage.setItem('selectSeanse', JSON.stringify(selectSeanse))
+    localStorage.setItem('selectSeanse', JSON.stringify(selectSeanse));
     const link = document.createElement('a');
     link.href = "payment.html";
     link.click();
-  })
+  });
 
 
 });
