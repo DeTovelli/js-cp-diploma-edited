ec9587908b98919d8f4a478744134a9aa3e347ee
DeTovelli <detovelli@gmail.com>
2023-05-19 01:53:26 +0300

    fix_19.05_2

--- js/createRequest.js
+++ js/createRequest.js
@@ -4,23 +4,23 @@ const createRequest = (options = {}) => {
   xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
   xhr.send(options.params);
   xhr.onreadystatechange = () => {
-    if (xhr.readyState === 4) {
-      if (xhr.status === 200) {
-        try {
-          const response = JSON.parse(xhr.responseText);
-          for (let key in response) {
-            if (response[key].err) {
-              alert(`Ошибка обращения к базе данных ${response[key].err}: ${response[key].errMessage}`);
-              return;
-            }
-          }
-          options.callback(response);
-        } catch (err) {
-          alert(`Ошибка разбора ответа сервера: ${err.message}`);
-        }
-      } else {
-        alert(`Ошибка запроса к серверу: ${xhr.status} ${xhr.statusText}`);
-      }
-    }
-  };
+		if (xhr.readyState === 4) {
+			if (xhr.status === 200) {
+				try {
+					const response = JSON.parse(xhr.responseText);
+					for (let key in response) {
+						if (response[key].err) {
+							alert(`Ошибка обращения к базе данных ${response[key].err}: ${response[key].errMessage}`);
+							return;
+						}
+					}
+					options.callback(response);
+				} catch (err) {
+					alert(`Ошибка ответа: ${err.message}`);
+				}
+			} else {
+				alert(`Ошибка запроса: ${xhr.status} ${xhr.statusText}`);
+			}
+		}
+	};
 };
